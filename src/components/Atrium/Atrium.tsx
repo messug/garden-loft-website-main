d6f1d99809426f60856d7c1a0eadf310eff7aa84
whimsical-phoenix <shahprapti12@gmail.com>
2024-06-10 17:48:18 -0600

    Atrium style and other changes

--- src/components/Atrium/Atrium.tsx
+++ src/components/Atrium/Atrium.tsx
@@ -1,8 +1,134 @@
 
+// import './Atrium.css';
+// import Navbar from '../Navbar/Navbar';
+// import Footer from '../Footer/Footer';
+// // import GLHero from '/images/hero-atrium.mp4';
+// import AtriumImage1 from '/images/Atrium 1.jpg';
+// import AtriumImage2 from '/images/Atrium 2.jpg';
+// import AtriumImage3 from '/images/Atrium 3.jpg';
+// import AtriumImage4 from '/images/Atrium 4.jpg';
+// import AtriumImage5 from '/images/Atrium 5.jpg';
+// import AtriumImage6 from '/images/Atrium 6.jpg';
+// import AtriumImage7 from '/images/Atrium 7.jpg';
+// import AtriumImage8 from '/images/Atrium 8.jpg';
+// import BrendaStrafford from '/images/BrendaStraffordLogo.png';
+// import GLLogo from '../../assets/gl-john-logo.png';
+
+// const Atrium = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <section className="hero-atrium">
+//         <div className="hero-atrium-img">
+//           {/* <video className="hero-video" src={GLHero} autoPlay loop muted /> */}
+//           <img src={AtriumImage1} alt="Atrium House Image 1" />
+
+//           <div className="hero-content">
+//             <h1>Atrium House</h1>
+//             <p>care enhanced  <br/>community living</p>
+//           </div>
+//         </div>
+//       </section>
+
+
+
+//       <section id="atrium">
+//         <div className="atrium-content">
+       
+          
+//           <h1>Combining advanced age-in-place architecture with person-led care and living support</h1>
+//           <h3>Atrium House features Garden Loft units clustered around a central interior light-filled community atrium</h3>
+//           <img src={AtriumImage2} alt="Atrium House Image 2" />
+          
+//           <h3>Atrium House is a small care home providing high levels of nursing care and support for up to 14 residents</h3>
+//           <img src={AtriumImage3} alt="Atrium House Image 3" />
+          
+//           <h3>The familiar domestic environment combines the privacy and independence of living in a Garden Loft with the social connectiveness and sense of belonging that comes from being part of a small village-style community.</h3>
+//           <button className='button-atrium'>Learn more about Garden Loft units</button>
+          
+//           <img src={AtriumImage4} alt="Atrium House Image 4" />
+//           <h3>The prefabricated Garden Loft units can be configured in any number of ways and combined with other uses to meet the site and programmatic needs of a particular project</h3>
+          
+//           <button className='button-atrium'> We custom design and build Atrium Houses across Alberta
+//           </button> 
+// <div className="logoDiv">
+// <img src={BrendaStrafford} alt="Brenda Strafford Logo" />
+//           <img src={GLLogo} alt="Garden Loft Logo" />
+          
+         
+//           </div>
+
+//           <h2>Atrium House is a collaboration with the Brenda Strafford Foundation</h2>
+          
+
+//         <div id='plusDiv'>
+//            <h3>advanced
+// age-in-place
+// architecture</h3>
+// <div className="plus-sign">+</div>
+// <h3>person-led
+// care and
+// operations</h3>
+// </div>
+//           <div className="foundation-section">
+       
+// <div className="foundation-box">
+//               <h2>GARDEN LOFT</h2>
+//               <ul>
+//                 <li>Research-based inclusive and accessible design</li>
+//                 <li>Precision adaptive prefabrication</li>
+//                 <li>Award-winning architectural expertise</li>
+               
+//               </ul>
+//             </div>
+          
+//             <div className="foundation-box">
+//               <h2>BRENDA STRAFFORD FOUNDATION</h2>
+//               <ul>
+//                 <li>Adaptive care and living model based on inclusion, diversity, and equity</li>
+//                 <li>Customized and guided operational playbook</li>
+//                 <li>Award-winning care expertise</li>
+//               </ul>
+//             </div>
+//           </div>
+
+//           <button className='button-atrium'>Call us</button>
+          
+//           <img src={AtriumImage5} alt="Atrium House Image 5" />
+//           <h3>Atrium House is particularly well suited for rural and remote communities</h3>
+//           <button className='button-atrium'>Book a preliminary consult</button>
+          
+//           <img src={AtriumImage6} alt="Atrium House Image 6" />
+//           <h3>Atrium House can also be combined with a Garden Loft Village</h3>
+          
+//           <img src={AtriumImage7} alt="Atrium House Image 7" />
+
+//           <h3>A common entry courtyard deck
+// enhances community by
+// connecting the private outdoor
+// spaces for the independent living
+// Garden Loft units with
+// the communal outdoor space for
+// Atrium residents</h3>
+
+// <img src={AtriumImage8} alt="Atrium House Image 8" />
+
+//           <button className='button-atrium'>Visit our showsuite and sales center</button>
+
+         
+//         </div>
+//       </section>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Atrium;
+
+
 import './Atrium.css';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-import GLHero from '/images/hero-atrium.mp4';
 import AtriumImage1 from '/images/Atrium 1.jpg';
 import AtriumImage2 from '/images/Atrium 2.jpg';
 import AtriumImage3 from '/images/Atrium 3.jpg';
@@ -19,64 +145,46 @@ const Atrium = () => {
     <>
       <Navbar />
       <section className="hero-atrium">
-        <div className="hero-content">
-          <video className="hero-video" src={GLHero} autoPlay loop muted />
-          <div className="content">
-            <h1>Garden Loft Atrium Houses</h1>
-            <p>care enhanced  <br/>community living</p>
+        <div className="hero-atrium-img">
+          <img src={AtriumImage1} alt="Atrium House Image 1" />
+          <div className="hero-atrium-content">
+            <h1>Atrium House</h1>
+            <p>care enhanced <br />community living</p>
           </div>
         </div>
       </section>
       <section id="atrium">
         <div className="atrium-content">
-        <img src={AtriumImage1} alt="Atrium House Image 1" />
-          
           <h1>Combining advanced age-in-place architecture with person-led care and living support</h1>
-          <h3>Atrium House features Garden Loft units clustered around a central interior light-filled community atrium</h3>
+          <h3><em>Atrium House features Garden Loft units clustered around a central interior light-filled community atrium</em></h3>
           <img src={AtriumImage2} alt="Atrium House Image 2" />
-          
-          <h3>Atrium House is a small care home providing high levels of nursing care and support for up to 14 residents</h3>
+          <h1>Atrium House is a small care home providing high levels of nursing care and support for up to 14 residents</h1>
           <img src={AtriumImage3} alt="Atrium House Image 3" />
-          
-          <h3>The familiar domestic environment combines the privacy and independence of living in a Garden Loft with the social connectiveness and sense of belonging that comes from being part of a small village-style community.</h3>
+          <h3><em>The familiar domestic environment combines the privacy and independence of living in a Garden Loft with the social connectiveness and sense of belonging that comes from being part of a small village-style community</em></h3>
           <button className='button-atrium'>Learn more about Garden Loft units</button>
-          
           <img src={AtriumImage4} alt="Atrium House Image 4" />
-          <h3>The prefabricated Garden Loft units can be configured in any number of ways and combined with other uses to meet the site and programmatic needs of a particular project</h3>
-          
-          <button className='button-atrium'> We custom design and build Atrium Houses across Alberta
-          </button> 
-<div className="logoDiv">
-<img src={BrendaStrafford} alt="Brenda Strafford Logo" />
-          <img src={GLLogo} alt="Garden Loft Logo" />
-          
-         
+          <h3><em>The prefabricated Garden Loft units can be configured in any number of ways and combined with other uses to meet the site and programmatic needs of a particular project</em></h3>
+          <img src={AtriumImage5} alt="Atrium House Image 5" />
+          <button className='button-atrium'>We custom design and build Atrium Houses across Alberta</button>
+          <div className="logoDiv">
+            <img src={BrendaStrafford} alt="Brenda Strafford Logo" />
+            <img src={GLLogo} alt="Garden Loft Logo" />
           </div>
-
-          <h2>Atrium House is a collaboration with the Brenda Strafford Foundation</h2>
-          
-
-        <div id='plusDiv'>
-           <h3>advanced
-age-in-place
-architecture</h3>
-<div className="plus-sign">+</div>
-<h3>person-led
-care and
-operations</h3>
-</div>
+          <h1>Atrium House is a <br/>collaboration with <br/>the Brenda Strafford<br/> Foundation</h1>
+          <div id='plusDiv'>
+            <h3>advanced<br/> age-in-place <br/>architecture</h3>
+            <div className="plus-sign">+</div>
+            <h3>person-led <br/>care and<br/> operations</h3>
+          </div>
           <div className="foundation-section">
-       
-<div className="foundation-box">
+            <div className="foundation-box">
               <h2>GARDEN LOFT</h2>
               <ul>
                 <li>Research-based inclusive and accessible design</li>
                 <li>Precision adaptive prefabrication</li>
                 <li>Award-winning architectural expertise</li>
-               
               </ul>
             </div>
-          
             <div className="foundation-box">
               <h2>BRENDA STRAFFORD FOUNDATION</h2>
               <ul>
@@ -86,31 +194,30 @@ operations</h3>
               </ul>
             </div>
           </div>
-
           <button className='button-atrium'>Call us</button>
-          
-          <img src={AtriumImage5} alt="Atrium House Image 5" />
-          <h3>Atrium House is particularly well suited for rural and remote communities</h3>
+          <img src={AtriumImage6} alt="Atrium House Image 6" />
+          <h1>Atrium House is<br/> particularly well suited<br/> for rural and remote<br/> communities</h1>
+          <h3><em>Atrium House’s prefabricated
+modular construction system
+can be deployed in areas with
+limited access to construction
+materials and trades</em></h3>
+<h3><em>The Atrium Way commissioning
+playbook can be customized and
+implemented to meet
+the capacity and capabilities of the
+service provider.</em></h3>
+<h3><em>The result is a one-stop-shop for
+designing, fabricating, and
+launching high-quality care
+enhanced housing solutions</em></h3>
           <button className='button-atrium'>Book a preliminary consult</button>
           
-          <img src={AtriumImage6} alt="Atrium House Image 6" />
-          <h3>Atrium House can also be combined with a Garden Loft Village</h3>
-          
+          <h3>Atrium House can also <br/>be combined with a <br/>Garden Loft Village</h3>
           <img src={AtriumImage7} alt="Atrium House Image 7" />
-
-          <h3>A common entry courtyard deck
-enhances community by
-connecting the private outdoor
-spaces for the independent living
-Garden Loft units with
-the communal outdoor space for
-Atrium residents</h3>
-
-<img src={AtriumImage8} alt="Atrium House Image 8" />
-
+          <h3><em>A common entry courtyard deck enhances community by connecting the private outdoor spaces for the independent living Garden Loft units with the communal outdoor space for Atrium residents</em></h3>
+          <img src={AtriumImage8} alt="Atrium House Image 8" />
           <button className='button-atrium'>Visit our showsuite and sales center</button>
-
-         
         </div>
       </section>
       <Footer />
