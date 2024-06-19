c88c7772ee4612bf98279b59cc69495cbcb3a123
Meseret <mesiteklu2018@gmail.com>
2024-06-19 11:31:48 -0600

    added style

--- src/components/Award/Award.tsx
+++ src/components/Award/Award.tsx
@@ -3,33 +3,21 @@ import "./Award.css";
 const Award = () => {
   return (
     <>
-      <section className="award-section" id="award">
-        <div className="award-content">
-          <h1>
-            Garden Loft received the 2024 City of Calgary Accessibility Award
-          </h1>
-          <div className="learn-btn">
-            <button className="learn-more-btn">Learn More</button>
-          </div>
+    <section className="award-section"  id="award">
+      <div className="award-content">
+        <h1>
+          Garden Loft received the 2024 City of Calgary Accessibility Award
+        </h1>
+        <div className="learn-btn">
+          <button className="learn-more-btn">Learn More</button>
         </div>
-        <div className="video-container">
-          <iframe
-            width="680"
-            height="330"
-            src="https://www.youtube.com/embed/y_nhYxDIm24" // Use the correct YouTube embed URL
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
-      </section>
-      <div style={{ textAlign: 'center', marginTop: '20px' }}>
-        <button className="award-button">
-          Learn more <br />about our <br /> awards
-        </button>
       </div>
+    </section>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <button className="award-button">Learn more <br/>about our <br/> awards</button>
+    </div>
     </>
   );
-};6
+};
+
 export default Award;
