7b2c0616c7f87b9e00b8a6162743d2ec4bb9c4cb
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-06-21 06:37:17 -0600

    push samantha

--- src/components/Safety/Safety.tsx
+++ src/components/Safety/Safety.tsx
@@ -10,6 +10,10 @@ const Safety: React.FC = () => {
         <h1 className='ideal'>Ideal for seniors and those with differing abilities</h1>
         <button className='button-safety'><Link to="/learn-more#safety-support" >Learn about <br />our support <br />features</Link></button>
     </div>
+    <div className="samantha">
+    <h1><em>“The team who designed this unit had people’s real interests and needs in mind, they involved accessibility experts and people with lived experience, and it shows. They’re paving the way for accessible, adaptable and affordable housing options that are so badly needed across the country.”</em> </h1>
+<h1> - Samantha Ryan C.E.T., RHFAC, BCIN Principal, Accessible Built Environment (ABE) Specialist ABE Factors Inc.</h1>
+    </div>
     </section>
   );
 };
