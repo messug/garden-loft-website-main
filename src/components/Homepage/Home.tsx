9cff83373b93645ebb5e79bf0dfba5b1ce447c8f
Meseret <mesiteklu2018@gmail.com>
2024-07-22 16:04:13 -0600

    changes for tab

--- src/components/Homepage/Home.tsx
+++ src/components/Homepage/Home.tsx
@@ -13,7 +13,7 @@ const Home: React.FC = () => {
           <video className="video" src={HomeVideo} autoPlay loop muted playsInline  />
           <div className="content">
             <p className='mobile-para'>Supportive housing <br />you want to live in</p>
-            <p className='desktop-para'>Supportive housing you want to live in</p>
+            <p className='desktop-para'>Supportive housing  <br />you want to live in</p>
           </div>
         </div>
       </section> 
@@ -35,7 +35,7 @@ const Home: React.FC = () => {
     
     <div className="item-container">
       <div className="image-container">
-        <img src="/images/Backyard Circle_1.png" alt="Village" />
+        <img src="/images/village Circle_1.png" alt="Village" />
       </div>
       <div className="text-container">
         <h3><a href="/villages">Villages</a></h3>
@@ -54,6 +54,46 @@ const Home: React.FC = () => {
     </div>
   </div>
 </section>
+
+<section className='desktop-homepage'>
+  <div className='deskpage-content'>
+    <h1>Garden loft</h1>
+    <p>is a prefabricated modular <br /> system for creating  <br />small-scale high-quality <br /> housing developments</p>
+    <p>We simplify the process of <br /> creating joyously supportive <br />places to live</p>
+    
+    <div className="desk-container">
+      <div className="image-container">
+        <img src="/images/Backyard Circle_1.png" alt="Backyards Suites" />
+      </div>
+      <div className="para-container">
+        <h3><a href="/mainpage">Backyard Suites</a></h3>
+        <p>family focused <br />supportive living</p>
+      </div>
+    </div>
+    
+    <div className="desk-container">
+      <div className="image-container">
+        <img src="/images/village Circle_1.png" alt="Village" />
+      </div>
+      <div className="para-container">
+        <h3><a href="/villages">Villages</a></h3>
+        <p>community embedded  <br />supportive living</p>
+      </div>
+    </div>
+    
+    <div className="desk-container">
+      <div className="image-container">
+        <img src="/images/Atrium House Circle_1.png" alt="Atrium House" />
+      </div>
+      <div className="para-container">
+        <h3><a href="/atrium">Small Care Homes</a></h3>
+        <p>care enhanced <br /> community living</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
 
 <Footer />
     </>
